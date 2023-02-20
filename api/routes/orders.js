0ddbe4c0c96617cbe98f9e898f153781e3d70107
eldar2malkic <88284877+eldar2malkic@users.noreply.github.com>
2023-02-19 23:30:24 -0600

    Update orders.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -13,7 +13,7 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.get('/:orderId', (req, res, next) => {
+routers.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order details',
         orderId: req.params.orderId
